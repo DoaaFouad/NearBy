7b986253e19229a72efaf40788cc0fba3e7b7cf9
Doaa Fouad <doaa_fouad2006@hotmail.com>
2019-10-26 14:00:44 +0200

    Add fused location provider

--- app/src/main/java/com/doaa/nearby/util/Constants.kt
+++ app/src/main/java/com/doaa/nearby/util/Constants.kt
@@ -18,5 +18,13 @@ class Constants {
         const val CLIENT_ID = "0OR3HH4OYW2FR2XNMZJ51QHDAYUJFXBPWYUTKVHGV0BX03GL"
         const val CLIENT_SECRET = "4ZDETA0ZZQOV5RC4PJH15DCNC1TXYU0CUMLOGOLE24SSTQ0K"
         const val VERSIONING = "20191024" //TODO let this automatically according to today's date
+
+        //Permissions
+        const val MY_PERMISSIONS_REQUEST_FINE_LOCATION = 2000
+
+        //Config
+        const val LOCATIONS_UPDATES_INTERVAL = 10000L
+        const val LOCATIONS_UPDATES_FATEST_INTERVAL = 5000L
+
     }
 }
--- app/src/main/java/com/doaa/nearby/view/HomeActivityView.kt
+++ app/src/main/java/com/doaa/nearby/view/HomeActivityView.kt
@@ -9,12 +9,18 @@
 
 package com.doaa.nearby.view
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Looper
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.doaa.nearby.R
 import com.doaa.nearby.adapter.PlaceAdapter
-import com.doaa.nearby.model.Location
 import com.doaa.nearby.model.response.PlacesResponse
+import com.doaa.nearby.util.Constants
 import com.doaa.nearby.viewmodel.HomeActivityViewModel
+import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.home_activity.*
 import org.koin.android.ext.android.inject
 
@@ -24,16 +30,17 @@ class HomeActivityView : BaseActivity<HomeActivityViewModel>() {
     override val layoutRes = R.layout.home_activity
 
     private val placeAdapter = PlaceAdapter()
+    //Location services
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
 
     override fun initViews() {
         setUpPlacesRecyclerViewer()
+        setupLocationProvider()
     }
 
     override fun observeViewModel() {
-        viewModel.requestNearbyPlaces(Location(31.2717393, 30.0159572)).observe({
-            this.lifecycle
-        }, { handlePlacesData(it) })
-
         viewModel.isLoading().observe({
             this.lifecycle
         }, { if (it) showLoading() else hideLoading() })
@@ -54,6 +61,67 @@ class HomeActivityView : BaseActivity<HomeActivityViewModel>() {
         placeAdapter.setData(places.response?.groups?.get(0)?.items)
     }
 
+    private fun requestToFetchPlaces(location: android.location.Location?) {
+        location?.let {
+            viewModel.requestNearbyPlaces(com.doaa.nearby.model.Location(it.latitude, it.longitude))
+                .observe({
+                    this.lifecycle
+                }, { places -> handlePlacesData(places) })
+        } ?: kotlin.run {
+            //TODO SOMETHING WRONG HAPPENED
+        }
+    }
+
+    /**
+     * Setup locations services
+     * */
+    private fun setupLocationProvider() {
+        checkUserPermissions()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequestConfig()
+        setLocationCallBack()
+    }
+
+    private fun locationRequestConfig(): LocationRequest? {
+        return LocationRequest.create()?.apply {
+            interval = Constants.LOCATIONS_UPDATES_INTERVAL
+            fastestInterval = Constants.LOCATIONS_UPDATES_FATEST_INTERVAL
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+
+    private fun setLocationCallBack() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                requestToFetchPlaces(locationResult.lastLocation)
+            }
+        }
+    }
+
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(
+            locationRequestConfig(),
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    private fun checkUserPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // if permission not guranted, then request
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+                Constants.MY_PERMISSIONS_REQUEST_FINE_LOCATION
+            )
+        } else {
+            // permission is guaranteed do nothing
+        }
+    }
+
     private fun showLoading() {
 
     }
@@ -70,4 +138,9 @@ class HomeActivityView : BaseActivity<HomeActivityViewModel>() {
 
     }
 
+    override fun onResume() {
+        super.onResume()
+
+        startLocationUpdates()
+    }
 }
